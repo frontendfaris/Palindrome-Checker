71b683ea7b75dc60491e7b58298060c7cf02837c
frontendfm <137447016+frontendfaris@users.noreply.github.com>
2024-08-29 02:36:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const userInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const resultContainer = document.getElementById("result");
+const funFactButton = document.getElementById("funfact-btn");
 
 function checkTheInput(input) {
   const theirInput = input;
@@ -31,6 +32,20 @@ function checkTheInput(input) {
   resultContainer.classList.remove("hidden");
 }
 
+const funFactPress = () => {
+  resultContainer.replaceChildren();
+
+  let funMsg = `The longest palindrome sentence in the English language is a phrase containing 74,633 letters arranged into 17,826 words!
+  \n Starting with: "A man, a plan, a cameo", and ending with: "OEM, a canal, Panama!"`;
+
+  const pFunFact = document.createElement("p");
+  pFunFact.className = "fun-fact-p";
+  pFunFact.innerHTML = funMsg;
+  resultContainer.appendChild(pFunFact);
+
+  resultContainer.classList.remove("hidden");
+};
+
 checkButton.addEventListener("click", () => {
   checkTheInput(userInput.value);
   userInput.value = "";
@@ -41,4 +56,8 @@ userInput.addEventListener("keydown", (key) => {
     checkTheInput(userInput.value);
     userInput.value = "";
   }
+});
+
+funFactButton.addEventListener("click", () => {
+  funFactPress();
 });
